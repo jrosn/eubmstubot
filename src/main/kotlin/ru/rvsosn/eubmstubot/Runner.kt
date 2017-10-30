f7c8ac53060ee16519b8d3324fb24dcb56a90355
Roman Sosnovsky <rvsosnovsky@gmail.com>
2017-10-30 04:06:37 +0300

    Проведена kotlin'изация pom.xml

---
+++ src/main/kotlin/ru/rvsosn/eubmstubot/Runner.kt
@@ -0,0 +1,24 @@
+package ru.rvsosn.eubmstubot
+
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.TelegramBotsApi
+import org.telegram.telegrambots.exceptions.TelegramApiException
+import ru.rvsosn.eubmstubot.eubmstu.EUBmstuApiExecutor
+
+fun main(args: Array<String>) {
+    ApiContextInitializer.init()
+    val telegramBotsApi = TelegramBotsApi()
+    try {
+        val apiExecutor = EUBmstuApiExecutor(
+                FirefoxDriver(),
+                System.getProperty("ru.rvsosn.eubmstubot.eulogin"),
+                System.getProperty("ru.rvsosn.eubmstubot.eupassword"))
+        apiExecutor.warmingCache()
+
+        telegramBotsApi.registerBot(EuBmstuBot(apiExecutor))
+    } catch (e: TelegramApiException) {
+        e.printStackTrace()
+    }
+
+}
